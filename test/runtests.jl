99280600d04c640d93c9041df791d3e2d7ccf21f
Samer <samerweb3@gmail.com>
2025-01-10 16:49:13 +0100

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,20 @@ using Test
 using SymplecticIntegrator
 
 @testset "Hamiltonian Validation Tests" begin
-	valid_hamiltonian(q::Vector{Float64}, p::Vector{Float64})::Float64 = 0.0
-	@test SymplecticIntegrator._validate_hamiltonian(valid_hamiltonian) == true
+	# function to test
+	validate_hamiltonian = SymplecticIntegrator._validate_hamiltonian
 
+	# valid hamiltonian
+	valid_hamiltonian(q::Vector{Float64}, p::Vector{Float64})::Float64 = 0.0
+	@test validate_hamiltonian(valid_hamiltonian) == true
+
+	# invalid hamiltonian
 	invalid_h1() = 0.0
-	@test_throws ArgumentError SymplecticIntegrator._validate_hamiltonian(invalid_h1)
+	@test_throws ArgumentError validate_hamiltonian(invalid_h1)
 
 	invalid_h2(q::Float64) = 0.0
-	@test_throws ArgumentError SymplecticIntegrator._validate_hamiltonian(invalid_h2)
+	@test_throws ArgumentError validate_hamiltonian(invalid_h2)
 
 	invalid_h3(q::Vector{Float64}, p::Vector{Float64})::Int = 0
-	@test_throws ArgumentError SymplecticIntegrator._validate_hamiltonian(invalid_h3)
+	@test_throws ArgumentError validate_hamiltonian(invalid_h3)
 end
