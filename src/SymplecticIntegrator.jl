af1c2767a24de4222d277f1515bd9494c621c2e6
Samer <samerweb3@gmail.com>
2025-01-13 17:27:33 +0100

    validation, derive H eqns

--- src/SymplecticIntegrator.jl
+++ src/SymplecticIntegrator.jl
@@ -1,7 +1,42 @@
 module SymplecticIntegrator
 
+using Symbolics
+
+# Derive the Hamiltonian eqns and return them as Julia expressions
+function _derive_hamiltonian_eqns(H::Function, dims)
+	# Validate Hamiltonian
+	_validate_hamiltonian(H)
+
+	# Declare symbolic variables
+	@variables qs[1:dims] ps[1:dims]
+
+	# Declare symbolic expr
+	H_ = H(qs, ps)
+
+	# Declare differentiator
+	diff(expr::Num, var::Num) = expand_derivatives(Differential(var)(expr))
+
+	# Derive Hamiltonian eqns and build julia expressions
+	q̇s = [
+		eval(build_function(diff(H_, ps[i]), qs, ps))
+		for i in 1:dims
+	]
+
+	ṗs = [
+		eval(build_function(-diff(H_, qs[i]), qs, ps))
+		for i in 1:dims
+	]
+
+	return [q̇s, ṗs]
+end
+
 # Validation function for the user provided Hamiltonian
 function _validate_hamiltonian(H::Function)
+	# Check H has only 1 method
+	if length(methods(H)) != 1
+		throw(ArgumentError("Hamiltonian must have only one method."))
+	end
+
 	# Get first method
 	H_ = methods(H)[1]
 
@@ -17,17 +52,17 @@ function _validate_hamiltonian(H::Function)
 	arg_types = arg_types[2:end] # First arg is typeof(H)
 
 	# Check arg types
-	if arg_types != (Vector{Float64}, Vector{Float64})
-		throw(ArgumentError("Hamiltonian must accept two arrays of numeric types."))
+	if !(arg_types[1] <: AbstractVector{<:Number} && arg_types[2] <: AbstractVector{<:Number})
+		throw(ArgumentError("Hamiltonian must accept two AbstractVector{<:Number} arguments."))
 	end
 
-	# Get return type
+	# Get return type (use concrete arg types so Julia compiler can infer return type)
 	return_types = Base.return_types(H, (Vector{Float64}, Vector{Float64}))
 	return_type = return_types[1]
 
 	# Check return type
-	if !(return_type == Float64)
-		throw(ArgumentError("Hamiltonian must return a numeric Float64."))
+	if !(return_type <: Number)
+		throw(ArgumentError("Hamiltonian must return a Number."))
 	end
 
 	return true
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,48 @@
 using Test
 using SymplecticIntegrator
 
+# valid hamiltonians
+function valid_hamiltonian(q::AbstractVector{<:Number}, p::AbstractVector{<:Number})::Number
+	sum = 0
+	for i in eachindex(q)
+		sum += q[i] * p[i]
+	end
+	return sum
+end
+
+
+# invalid hamiltonians
+function invalid_hamiltonian(q::AbstractVector{<:Number}, p::AbstractVector{<:Number}, x::Number)::Number
+	sum = 0
+	for i in eachindex(q)
+		sum += q[i] * p[i] * x
+	end
+	return sum
+end
+
 @testset "Hamiltonian Validation Tests" begin
 	# function to test
 	validate_hamiltonian = SymplecticIntegrator._validate_hamiltonian
 
-	# valid hamiltonian
-	valid_hamiltonian(q::Vector{Float64}, p::Vector{Float64})::Float64 = 0.0
+	# valid hamiltonians
 	@test validate_hamiltonian(valid_hamiltonian) == true
 
+	# invalid hamiltonians
+	@test_throws ArgumentError validate_hamiltonian(invalid_hamiltonian)
+end
+
+@testset "Hamiltonian Eqns Derivation Tests" begin
+	# function to test
+	derive_hamiltonian_eqns = SymplecticIntegrator._derive_hamiltonian_eqns
+
+	# dims
+	dims = 3
+
+	# valid hamiltonian
+	result = derive_hamiltonian_eqns(valid_hamiltonian, dims)
+	@test length(result[1]) == 3
+	@test length(result[2]) == 3
+
 	# invalid hamiltonian
-	invalid_h1() = 0.0
-	@test_throws ArgumentError validate_hamiltonian(invalid_h1)
-
-	invalid_h2(q::Float64) = 0.0
-	@test_throws ArgumentError validate_hamiltonian(invalid_h2)
-
-	invalid_h3(q::Vector{Float64}, p::Vector{Float64})::Int = 0
-	@test_throws ArgumentError validate_hamiltonian(invalid_h3)
+	@test_throws ArgumentError derive_hamiltonian_eqns(invalid_hamiltonian, dims)
 end
